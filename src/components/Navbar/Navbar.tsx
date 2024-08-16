e263f0f8a3e9add1037ac257ab400aa4bf5bc1bd
Ali Hashemi <alihashemi1381.ah@gmail.com>
2024-08-16 11:45:36 +0330

    add logo

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,12 +2,16 @@ import { FC } from "react";
 import MobileNavbar from "./MobileNavbar/MobileNavbar";
 import { BiSearch } from "react-icons/bi";
 import Link from "next/link";
+import Image from "next/image";
+import Logo from "/public/Logo/BlueLogo.png";
 
 const Navbar: FC = () => {
   return (
     <div className="p-5">
       <div className="flex justify-between items-center">
-        <div>Powerd by NextJS</div>
+        <div className="mr-5 flex items-center">
+          <Image src={Logo.src} alt="" width={70} height={70} />
+        </div>
         <div>
           <ul className="hidden lg:flex  ">
             <li className="mx-5">
